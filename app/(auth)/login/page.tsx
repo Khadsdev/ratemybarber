07daef7b3516d60651c6d4ff48d7e1afa7abc522
Khadsdev <36850593+Khadsdev@users.noreply.github.com>
2025-10-12 18:45:04 +0200

    i was able to fetch data from supabase. Can even add an imgURL to databse that will be fetched to the UI

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -9,6 +9,7 @@ interface Barber {
   description: string;
   state: string;
   city: string;
+  imageUrl: string;
   created_at: string;
 }
 
@@ -64,26 +65,33 @@ export default function Login() {
         </div>
         <div className="max-w-md mx-auto">
           Her skal det komme barbers
-          <div className="">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"></div>
-
-            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
-                <img className="rounded-t-lg" alt="" />
-              </a>
-              <div className="p-5">
+          {barbers.map((barber, key) => (
+            <div className="" key={key}>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {barber.name}
+              </div>
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"></h5>
+                  <img className="rounded-t-lg" alt="" src={barber.imageUrl} />
                 </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
-                <a
-                  href="#"
-                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  Read more
-                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      {barber.name}
+                    </h5>
+                  </a>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {barber.description}
+                  </p>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
